e2198bb2ce0f048cba06fadd99aa24bfcdc77427
Oeun Nuphea <nupheaoeun@gmail.com>
2026-03-06 09:01:36 +0700

    x

--- index.ts
+++ index.ts
@@ -1,5 +1,8 @@
 import express from "express";
 import {Application, Request, Response } from "express";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
 
 
 const app: Application = express();
@@ -11,6 +14,17 @@ const PORT = 4000
 app.get('/', (_req: Request, res: Response) => {
   res.json({ message: 'Primsa Operactions' })
 });
+
+app.post('/create', (req: Request, res: Response) => {
+    const {name, email} = req.body;
+
+    const user = prisma.user.create({
+        data:{
+            name,
+            email
+        }
+    })
+})
 
 app.listen(PORT, () => {
   console.log(`Server is Fire at http://localhost:${PORT}`);
