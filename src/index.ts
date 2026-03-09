98bc6cb4830fb694210108f3401e9a866c4011d5
Oeun Nuphea <nupheaoeun@gmail.com>
2026-03-09 15:41:56 +0700

    x

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
 import express, { Application } from "express";
 import user from "./routes/user.routes";
 import note from "./routes/note.routes";
+import cors from "cors";
+
 
 
 const app: Application = express();
 const PORT = 4000;
+
+app.use(cors());
 
 app.use(express.json());
 app.use("/users", user);
@@ -13,4 +17,6 @@ app.use("/notes", note);
 
 app.get("/", (_req, res) => res.json({ message: "Note is running" }));
 
-app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`));
+app.listen(PORT, "0.0.0.0", () => 
+  console.log(`Server running at http://0.0.0.0:${PORT}`)
+);
