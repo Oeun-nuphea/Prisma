6d4776919b06bcd0eff1c0e225c13affeea33958
Oeun Nuphea <nupheaoeun@gmail.com>
2026-03-12 13:45:25 +0700

    aupte

--- src/index.ts
+++ src/index.ts
@@ -50,7 +50,7 @@ app.use(
 );app.use(express.json({limit: "5mb"}));
 
 // ─── Clean Respone ──────────────────────────────────────────────────────────
-// app.use(sanitizeRequest);
+app.use(sanitizeRequest);
 
 
 // ─── Morgan Logging ──────────────────────────────────────────────────────────
