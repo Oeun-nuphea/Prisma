e06d77cf50bd3f7a3f69c3b05d5132aedd06f7a7
Oeun Nuphea <nupheaoeun@gmail.com>
2026-03-12 09:06:55 +0700

    update

--- src/index.ts
+++ src/index.ts
@@ -43,7 +43,7 @@ app.use(
 );
 app.use(cookieParser());
 app.use(helmet({ contentSecurityPolicy: false }));
-app.use(express.json({limit: "1mb"}));
+app.use(express.json({limit: "5mb"}));
 
 // ─── Morgan Logging ──────────────────────────────────────────────────────────
 if (process.env.NODE_ENV !== "production") {
